c6433974a040c52204189adecd1dfde7b3c3c3d6
erinallan00 <Guest@stk-cs01-ws02.westlothian.sch.uk>
2015-12-16 15:02:05 +0000

    Sprint1.4

---
+++ ORDERS.java
@@ -0,0 +1,39 @@
+public class ORDERS
+{
+  private String customerID;
+  private String ticketID;
+  private int NoOfTickets;
+  private String MethodOfPurchase;
+
+  public ORDERS()
+  {
+    customerID = "";
+    ticketID = "";
+    NoOfTickets = 0;
+    MethodOfPurchase = "";
+  }
+
+  public void readOrderDetails(String dataItems) 
+  {
+    String[] rowItems = dataItems.split(",");
+    customerID = rowItems[0];
+    ticketID = rowItems[1];
+    NoOfTickets = Integer.parseInt(rowItems[2]);
+    MethodOfPurchase = (rowItems[3]);
+  }
+  public String writeDetails()
+  {
+      String orderData = "";
+        orderData = orderData.concat(customerID);
+        orderData = orderData.concat(",");
+        orderData = orderData.concat(ticketID);
+        orderData = orderData.concat(",");
+        orderData = orderData.concat(Integer.toString(NoOfTickets));
+        orderData = orderData.concat(MethodOfPurchase);
+        orderData = orderData.concat(",");
+        return orderData;
+  }
+  
+  
+   
+}
