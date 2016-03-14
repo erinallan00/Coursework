40884a303437c61d9f98f2bae1ebf28066af397a
Guest User <Guest@stk-cs01-ws11.westlothian.sch.uk>
2016-03-14 04:40:05 -0700

    4

--- SCHOOL.java
+++ SCHOOL.java
@@ -38,6 +38,22 @@ public class SCHOOL
 
     public void calculateMethodOfSales()
     {
+        int SchoolSales = 0;
+        int OnlineSales = 0;
+        for (int i = 0; i < noOfOrders; i++)
+        {
+            // decide if customer bought ticket in school.
+            if ((OrderList[i].getMethodOfPurchase().equals("S")))
+            {
+                SchoolSales = SchoolSales +1;
+            }
+            //count all S values then take away from 300 to find O values.
+            //use an if statement to decide which is most popular method of sales.
+            // if CountSchoolSales is greater than CountOnlineSales then CountSchoolSales is the most popular, if CountOnlineSales is greater than CountSchoolSales then CountOnlineSales is most popular.
+            OnlineSales = NoOfOrders - SchoolSales;
+            if ((
+            System.out.println("\n The most popular method of sales is : " + );
+        }
 
     }
 
